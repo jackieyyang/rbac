ae2aaf7afda3109b89a2103b0204bdfcdba0514f
jackieyyang <yang192827@163.com>
2024-04-25 01:30:07 +0800

    exclude knife4j path in the RbacFilter

--- src/main/java/com/jackieyang/rbac/filter/RbacFilter.java
+++ src/main/java/com/jackieyang/rbac/filter/RbacFilter.java
@@ -6,9 +6,12 @@ import com.jackieyang.rbac.service.RbacService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Component
 @RequiredArgsConstructor
 public class RbacFilter implements HandlerInterceptor {
 
@@ -16,6 +19,11 @@ public class RbacFilter implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        // exclude specific request path
+        if (isExclude(request.getRequestURI())) {
+            return true;
+        };
 
         // pass if contains "RbacIgnore" in the request method
         if (handler instanceof HandlerMethod handlerMethod) {
@@ -31,5 +39,17 @@ public class RbacFilter implements HandlerInterceptor {
         // authorization check
         rbacService.authorization(request.getRequestURI(), request.getMethod(), userRolePermission);
         return true;
+
+    }
+
+    private Boolean isExclude(String servletPath) {
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+        if (antPathMatcher.match("/doc.html", servletPath) ||
+            antPathMatcher.match("/webjars/**", servletPath) ||
+            antPathMatcher.matchStart("/favicon.ico", servletPath) ||
+            antPathMatcher.match("/v3/api-docs/**", servletPath)) {
+            return true;
+        }
+        return false;
     }
 }
